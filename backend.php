ee622e1837e54dc0889eb7e0bdc84e3ef75c5ed4
IH-honnette <ihozohonnette12@gmail.com>
2023-01-24 09:31:31 +0200

    Add backend file to send request to microcontroller

---
+++ backend.php
@@ -0,0 +1,34 @@
+<?php
+
+if($_POST != null){
+    if($_POST["state"] == "on"){
+        writeOne();
+    }else{
+        writeZero();
+    }
+}
+
+//writeOne function
+
+function writeOne(){
+    $file = fopen("bool.txt", "w");
+    fwrite($file, '1');
+    fclose($file);
+}
+
+//writeZero function
+function writeZero(){
+    $file = fopen("bool.txt", "w");
+    fwrite($file, '0');
+    fclose($file);
+}
+
+function isOn(){
+    $string = file_get_contents("bool.txt");
+    if($string == "1"){
+        return true;
+    }
+    return false;
+}
+
+?>
